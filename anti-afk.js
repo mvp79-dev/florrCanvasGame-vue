1845c0d7eb3d10c3011e56d4e0758c8ae2608ac4
unicorn79dev <b_norene81@outlook.com>
2024-07-15 14:51:52 -0700

    Create anti-afk.js

---
+++ anti-afk.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name Anti-AFK
+// @author kit2d2
+// @namespace Tampermonkey Scripts
+// @match https://florr.io
+// @grant none
+// ==/UserScript==
+
+(function() {
+    function clickImHereButton() {
+        var buttons = document.querySelectorAll('button'); 
+        buttons.forEach(function(button) {
+            if (button.textContent.trim().toLowerCase() === "I'm here") {
+                button.click();
+            }
+        });
+    }
+
+    function getRandomTime(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    var delay = getRandomTime(2001, 5005);
+    setTimeout(function() {
+        clickImHereButton();
+    }, delay);
+})();
