c24773fa1c81bb4c794ed086d8b10853a70a84e5
unicorn79dev <b_norene81@outlook.com>
2024-07-25 09:36:30 -0700

    Update anti-afk.js

--- anti-afk.js
+++ anti-afk.js
@@ -5,23 +5,40 @@
 // @match https://florr.io/*
 // @grant none
 // ==/UserScript==
-// broken atm since i used html butotns im kindof dumb
-/*(function() {
-    function clickImHereButton() {
-        var buttons = document.querySelectorAll('button'); 
-        buttons.forEach(function(button) {
-            if (button.textContent.trim() === "I'm here") { // case sens
-                button.click();
-            }
-        });
+    let bd = buttonData[c[0]]
+    if(bd && (!bd.color || bd.color === b.fillStyle) && (!bd.font || bd.font === b.font)) {
+      let t = untransform(c[1], c[2], b.getTransform())
+      let o = lbuttons[c[0]]
+      let n = buttons[c[0]] = {
+        x: t[0],
+        y: t[1],
+        d: 1,
+        s: performance.now(), // **WAIT BEFORE CLICKING A BUTTON** Removing this will
+                              // probably get you banned faster
+        fillStyle: b.fillStyle,
+        font: b.font
+      }
+      if(o) {
+        n.d = Math.abs(n.x - o.x) + Math.abs(n.y - o.y)
+        n.s = o.s
+      }
     }
-
-    function getRandomTime(min, max) {
-        return Math.random() * (max - min) + min;
+    if(c[0] === `I'm here` && client.bypassAfkCheck.afkButton) {
+      afkButton(untransform(c[1], c[2], b.getTransform()))
     }
-
-    var delay = getRandomTime(2001, 5005);
+    lastText = [c, b.fillStyle]
+    if(b.fillStyle === '#ffffff') {
+      lastWhiteText = [c, b.fillStyle]
+    }
+    c[0] = textTransform(c[0], b)
+    return Reflect.apply(a, b, c)
+  } })
+  let clicking = false
+  const afkButton = function(t) {
+    if(clicking) { return }
+    clicking = true
     setTimeout(function() {
-        clickImHereButton();
-    }, delay);
-})();*/
+      clicking = false
+      clickAt(t.x, t.y)
+    }, 500 + 2000 * Math.random())
+  }
